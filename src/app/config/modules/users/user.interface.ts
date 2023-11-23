454da12ffa083d276a417c35d3279eae3a50b892
alaminhossa1n <alaminatghoria@gmail.com>
2023-11-24 00:25:09 +0600

    model and interface created

---
+++ src/app/config/modules/users/user.interface.ts
@@ -0,0 +1,23 @@
+export type User = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: "active" | "inActive";
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  orders: {
+    productName: string;
+    price: number;
+    quantity: number;
+  }[];
+}; 
